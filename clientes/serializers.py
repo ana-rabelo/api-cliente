59edb5d1697003bb3a7aeb18a061aa8fc0c28475
Ana Paula Rabelo <prabelo79@gmail.com>
2024-06-10 10:19:24 -0300

    add: validação de campos de cliente

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -11,3 +11,17 @@ class ClienteSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('O CPF deve ter 11 dígitos')
         return cpf
     
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError('O nome deve conter apenas letras')
+        return nome
+    
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError('O RG deve ter 9 dígitos')
+        return rg
+     
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError('O número de celular deve ter 11 dígitos, no mínimo')
+        return celular
