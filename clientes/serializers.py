98d1595b1b718c1c0b0dbb0b4b2a1091d167316e
Ana Paula Rabelo <prabelo79@gmail.com>
2024-06-10 10:33:36 -0300

    refactor: separa as validações dos campos

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,27 +1,23 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
 
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError('O CPF deve ter 11 dígitos')
-        return cpf
-    
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError('O nome deve conter apenas letras')
-        return nome
-    
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError('O RG deve ter 9 dígitos')
-        return rg
-     
-    def validate_celular(self, celular):
-        if len(celular) < 11:
-            raise serializers.ValidationError('O número de celular deve ter 11 dígitos, no mínimo')
-        return celular
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': 'O CPF deve ter 11 dígitos'})
+        
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': 'O nome deve conter apenas letras'})
+        
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': 'O RG deve ter 9 dígitos'})
+        
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular': 'O número de celular deve ter 11 dígitos, no mínimo'})
+        
+        return data
